7ca48e3c7aae00f801e007cf4d878d96563a2f8a
Javi Aranda <refugiero@hotmail.com>
2018-10-08 19:56:04 -0300

    cambios en el main, uso de cards e imagenes

--- main.php
+++ main.php
@@ -12,25 +12,106 @@
     <link href="https://fonts.googleapis.com/css?family=Quicksand:300,400" rel="stylesheet">
 </head>
 <body>
+    <div class="background-image"></div>
     <div class="container">
-    <div class="row">
-        <div class= "contenedor-fecha col-8">
-            <p class="texto">Nombre de la planta</p>
-            <p class="texto">White widow</p>
-        </div>
-    </div>
-
-        <img class="img-fondo" src="images/fondo-login.jpg" alt="">
-        <div class="circulo-gradiente">
-            <div class="circulo-principal">
-                <div class="texto-circulo">
-                    <img class="planta" src="images/planta.png" alt="planta">
-                    <p class="estado" >3 semanas</p>
-                    <p class="estado" >4 días</p>
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class= "card contenedor-fecha text-center mb-5 mt-3">
+                    <p class="texto">Especie: White widow</p>
+                    <p class="texto">7 semanas y 4 días</p>
                 </div>
             </div>
         </div>
-          
+
+        <div class="card-deck">
+                <div class="card bg-light mb-3 text-center">
+                    <h5 class="card-header">Temperatura Interna</h5>
+                    <div class="card-body">
+                        <!-- <p class="card-title">Interna</p> -->
+                        <h1 class="card-text">25º</h1>
+                        <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted">Last updated 3 mins ago</small>
+                    </div>
+                </div>
+
+                <div class="card bg-light mb-3 text-center">
+                    <h5 class="card-header">Humedad Interna</h5>
+                    <div class="card-body">
+                        <!-- <h5 class="card-title">Humedad interna</h5> -->
+                        <h1 class="card-text">60%</h1>
+                        <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted">Last updated 3 mins ago</small>
+                    </div>
+                </div>
+
+                <div class="card bg-light mb-3 text-center">
+                    <h5 class="card-header">Fecha</h5>
+                    <div class="card-body">
+                        <!-- <h5 class="card-title">Humedad interna</h5> -->
+                        <h1 class="card-text">08/10/18</h1>
+                        <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted">Last updated 3 mins ago</small>
+                    </div>
+                </div>
+        </div>
+
+        <div class="card-deck">
+            <div class="card  text-success bg-dark mb-3 text-center">
+                <h5 class="card-header">Iluminación</h5>
+                <div class="card-body">
+                    <!-- <p class="card-title">Estado</p> -->
+                    <img class="lamp" src="images/lampara.png" alt="Card image cap">
+                    <h3 class="card-text text-success">Encendida</h3>
+                    <a href="#" class="btn btn-secondary">Apagar</a>
+                </div>
+                <!-- <div class="card-footer">
+                    <small class="text-muted">Last updated 3 mins ago</small>
+                </div> -->
+            </div>
+
+            <div class="card  text-success bg-dark mb-3 text-center">
+                <h5 class="card-header">Riego</h5>
+                <div class="card-body">
+                    <!-- <h5 class="card-title">Humedad interna</h5> -->
+                    <img class="lamp" src="images/riego.png" alt="Card image cap">
+                    <h3 class="card-text">Apagado</h3>
+                    <a href="#" class="btn btn-secondary">Encender</a>
+                </div>
+                <!-- <div class="card-footer">
+                    <small class="text-muted">Last updated 3 mins ago</small>
+                </div> -->
+            </div>
+
+            <div class="card text-success bg-dark mb-3 text-center">
+                <h5 class="card-header">Ventilador</h5>
+                <div class="card-body">
+                    <!-- <h5 class="card-title">Humedad interna</h5> -->
+                    <img class="lamp" src="images/ventilador.png" alt="Card image cap">
+                    <h3 class="card-text">Encendido</h3>
+                    <a href="#" class="btn btn-secondary">Apagar</a>
+                </div>
+                <!-- <div class="card-footer">
+                    <small class="text-muted">Last updated 3 mins ago</small>
+                </div> -->
+            </div>
+        </div>
+
+            <!-- <div class="circulo-gradiente"> -->
+                <!-- <div class="circulo-principal">
+                    <div class="texto-circulo">
+                        <img class="planta" src="images/planta.png" alt="planta">
+                        <p class="estado" >3 semanas</p>
+                        <p class="estado" >4 días</p>
+                    </div>
+                </div>
+            </div> -->
+            
     </div>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
