f052beb7f59bd47e5611c68f19c3aa5202d9c915
javi <refugiero@hotmail.com>
2019-08-08 23:01:26 -0300

    arreglo acceso

--- api/querys.php
+++ api/querys.php
@@ -2,9 +2,11 @@
 
 include_once("../clases/dbmysql.php");
 $db = new DBMySQL("../credenciales.json");
-
+ob_start();
+header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 $obj = json_decode($_GET["x"], false);
 $results = $db->generalQuery($obj);
 echo $results;
+ob_end_flush();
 ?>
